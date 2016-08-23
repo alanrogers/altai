fac5a2ebafdab1837a9440da96093657b2c74606
Alan R. Rogers <rogers@anthro.utah.edu>
2016-08-23 14:47:55 -0600

    Wrote filter.sh

--- modhum/filter.sh
+++ modhum/filter.sh
@@ -1,6 +1,8 @@
-ifile=$1
-ofile="AltaiNea.hg19_1000g.${chr}-filtered.bcf"
-ofile=`basename
+id=$1
+chr=$2
+base=${id}.hg19_1000g.${chr}
+ifile=${base}-fixed.vcf.bgz
+ofile=${base}-filtered.bcf
 bcftools view --output-file $ofile \
   -M2 --types snps --exclude 'QUAL<30 & MQ<30' \
   --regions-file ~/data/archbed/altai-denisova-map35_99-${chr}.bed.gz \
