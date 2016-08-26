97d8addf9b20711f43141d2546ea85cc34d6d8e8
Alan R. Rogers <rogers@anthro.utah.edu>
2016-08-26 12:23:52 -0600

    Revised filter.sh to use bedvcf.

--- modhum/filter.sh
+++ modhum/filter.sh
@@ -2,9 +2,7 @@ id=$1
 chr=$2
 base=${id}.hg19_1000g.${chr}
 ifile=${base}-fixed.vcf.bgz
-ofile=${base}-filtered.bcf
-bcftools view --output-file $ofile \
-  -M2 --types snps --exclude 'QUAL<30 & MQ<30' \
-  --regions-file ~/data/archbed/altai-denisova-map35_99-${chr}.bed.gz \
-  --output-type b \
-  $ifile
+ofile=${base}.filtered.vcf.gz
+bcftools view -M2 --types snps --exclude 'QUAL<30 || MQ<30' $ifile \
+ | bedvcf <(zcat ~/data/archbed/altai-denisova-map35_99-${chr}.bed.gz) \
+ | bgzip > ${ofile}
