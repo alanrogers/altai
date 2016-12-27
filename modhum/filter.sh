100231ad0faaac9aaf7713eb7b42705e0378b734
Alan R. Rogers <rogers@anthro.utah.edu>
2016-12-27 11:54:56 -0700

    Removed bed filter from modhum/filter.sh

--- modhum/filter.sh
+++ modhum/filter.sh
@@ -1,8 +1,10 @@
+# 2016-12-27: I'm removing the step that filters based on the archaic bed file.
+# This filtering happen automatically during generation of .daf file, so
+# it isn't necessary here.
 id=$1
 chr=$2
 base=${id}.hg19_1000g.${chr}
 ifile=${base}-fixed.vcf.bgz
 ofile=${base}.filtered.vcf.gz
 bcftools view -M2 --types snps --exclude 'QUAL<30 || MQ<30' $ifile \
- | bedvcf <(zcat ~/data/archbed/altai-denisova-map35_99-${chr}.bed.gz) \
  | bgzip > ${ofile}
