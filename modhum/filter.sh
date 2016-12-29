a406f0c4c26b0199334b18b50b9ab7021f82dd52
Alan R. Rogers <rogers@anthro.utah.edu>
2016-12-29 11:54:07 -0700

    Changed QC filters in modhum/filter.sh

--- modhum/filter.sh
+++ modhum/filter.sh
@@ -6,5 +6,7 @@ chr=$2
 base=${id}.hg19_1000g.${chr}
 ifile=${base}-fixed.vcf.bgz
 ofile=${base}.filtered.vcf.gz
-bcftools view -M2 --types snps --exclude 'QUAL<30 || MQ<30' $ifile \
+#bcftools view -M2 --types snps --exclude 'QUAL<30 || MQ<30' $ifile \
+# | bgzip > ${ofile}
+bcftools view --exclude 'QUAL<30 || MQ<30' $ifile \
  | bgzip > ${ofile}
