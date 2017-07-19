bf76e90f4f74748b6cccc12c44771da49417a0b5
Alan R. Rogers <rogers@anthro.utah.edu>
2017-07-19 15:39:59 -0600

    .

--- modhum/filter.sh
+++ modhum/filter.sh
@@ -1,6 +1,3 @@
-# 2016-12-27: I'm removing the step that filters based on the archaic bed file.
-# This filtering happen automatically during generation of .daf file, so
-# it isn't necessary here.
 id=$1
 chr=$2
 base=${id}.hg19_1000g.${chr}
@@ -10,7 +7,5 @@ ofile=${base}.filtered.vcf.gz
 # write seqid to stderr
 echo Processing sequence ${id}; chr ${chr} 1>&2
 
-#bcftools view -M2 --types snps --exclude 'QUAL<30 || MQ<30' $ifile \
-# | bgzip > ${ofile}
-bcftools view --exclude 'QUAL<30 || MQ<30' $ifile \
+bcftools view -M2 --types snps --exclude 'QUAL<30 || MQ<30' $ifile \
  | bgzip > ${ofile}
