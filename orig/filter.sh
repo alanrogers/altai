bf4224d076dad1d5f6b43430b4887e5506076113
Alan R. Rogers <rogers@anthro.utah.edu>
2017-09-30 16:07:10 -0600

    Add concat.slr

--- orig/filter.sh
+++ orig/filter.sh
@@ -1,8 +1,5 @@
 chr=$1
-ifile="AltaiNea.hg19_1000g.${chr}.vcf.bgz"
-ofile="AltaiNea.hg19_1000g.${chr}-filtered.bcf"
-bcftools view --output-file $ofile \
-  --types snps --exclude 'QUAL<30 & MQ<30' \
-  --regions-file ~/data/archbed/altai-denisova-map35_99-${chr}.bed.gz \
-  --output-type b \
-  $ifile
+bed="../bed/chr${chr}_mask.bed.gz"
+vcf="chr${chr}_mq25_mapab100.vcf.gz"
+ofile="vindija-chr${chr}-filtered.vcf.bgz"
+zcat ${vcf} | bedvcf <(zcat ${bed}) | bgzip > ${ofile}
